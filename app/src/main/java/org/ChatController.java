288015da999af1fe849006786f2f53c17754bc82
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-05-01 17:05:39 -0400

    initial commit

---
+++ app/src/main/java/org/ChatController.java
@@ -0,0 +1,17 @@
+package org;
+
+//import java.util.List;
+//
+//import javafx.collections.FXCollections;
+//import javafx.collections.ObservableList;
+//import javafx.event.ActionEvent;
+//import javafx.fxml.FXML;
+//import javafx.scene.control.Alert;
+//import javafx.scene.control.Alert.AlertType;
+//import javafx.scene.control.ChoiceBox;
+//import javafx.scene.control.ListView;
+//import javafx.scene.control.TextField;
+
+public class ChatController {
+  
+}
