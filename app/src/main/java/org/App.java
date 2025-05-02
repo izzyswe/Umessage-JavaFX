356f3cd58686358530e1b77569c2d000a3df70f6
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-05-02 01:45:25 -0400

    made ui more responsive

--- app/src/main/java/org/App.java
+++ app/src/main/java/org/App.java
@@ -10,11 +10,11 @@ public class App extends Application {
     @Override
 	  public void start(Stage primaryStage) {
 		  try {
-			  AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/PasswordSplashScreen.fxml"));
+			  AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/UmessageLogin.fxml"));
 			  Scene scene = new Scene(root,600,350);
 			  scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
 			  primaryStage.setScene(scene);
-        primaryStage.setTitle("Password Manager");
+        primaryStage.setTitle("Umessage");
 			  primaryStage.show();
 		  } catch(Exception e) {
 			  e.printStackTrace();
