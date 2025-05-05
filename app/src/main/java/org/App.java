3a574e89b12a62fe4d22782985ebebdab7be2137
Isaac Kim <isaackim@Isaacs-MacBook-Air.local>
2025-05-05 17:28:24 -0400

    CORRECTION: fixed ui sizing

--- app/src/main/java/org/App.java
+++ app/src/main/java/org/App.java
@@ -1,5 +1,6 @@
 package org;
 
+import javax.swing.text.PlainView;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -10,8 +11,25 @@ public class App extends Application {
     @Override
 	  public void start(Stage primaryStage) {
 		  try {
-			  AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/UmessageLogin.fxml"));
-			  Scene scene = new Scene(root,600,350);
+			  AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/UmessageSplashScreen.fxml"));
+			  Scene scene = new Scene(root,1000,820);
+        //splash screen
+        primaryStage.setResizable(false);
+        primaryStage.setOnHidden(e -> {
+            e.consume();
+        });
+        primaryStage.setOnShowing(e -> {
+            e.consume();
+          final Stage splashStage = new Stage(new PlainView(ele;
+        });
+        //set the timer to the splash screen for the main application
+        primaryStage.setOnShowing(e -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });
 			  scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
 			  primaryStage.setScene(scene);
         primaryStage.setTitle("Umessage");
